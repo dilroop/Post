40c4927f21ef709515ff7ce6748383b20e18781d
Dilroop Singh <Dilroop.singh@gmail.com>
2022-04-10 16:05:23 +1200

    Using Navigation Component for jetpack to setup navigation, created home screen called PostListFragment

--- app/src/main/java/com/dsb/post/ui/MainActivity.kt
+++ app/src/main/java/com/dsb/post/ui/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.dsb.post.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.dsb.post.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration : AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val host: NavHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment
+        ) as NavHostFragment? ?: return
+
+        val navController = host.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController = navController, appBarConfiguration)
     }
 }
---
+++ app/src/main/java/com/dsb/post/ui/posts/PostListViewModel.kt
@@ -0,0 +1,7 @@
+package com.dsb.post.ui.posts
+
+import androidx.lifecycle.ViewModel
+
+class PostListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
